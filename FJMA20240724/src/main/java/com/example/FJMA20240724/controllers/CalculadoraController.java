af144e6378d6d86c73b0f8ac9313ce7345c6c01d
fernando13522477@outlook.com <fernando13522477@outlook.com>
2024-07-25 09:50:23 -0600

    Se ha agregado funcionalidad y vista para multiplicacion

--- FJMA20240724/src/main/java/com/example/FJMA20240724/controllers/CalculadoraController.java
+++ FJMA20240724/src/main/java/com/example/FJMA20240724/controllers/CalculadoraController.java
@@ -25,4 +25,16 @@ public class CalculadoraController {
         model.addAttribute("result", result);
         return "calculadora/resta";
     }
+    @GetMapping("/multiplicar")
+    public String multiplicar(){
+        return "calculadora/multiplicar";
+    }
+    @PostMapping("/multiplicar")
+    public String performMul(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+        int result = num1 * num2;
+        model.addAttribute("num1", num1);
+        model.addAttribute("num2", num2);
+        model.addAttribute("result", result);
+        return "calculadora/multiplicar";
+    }
 }
